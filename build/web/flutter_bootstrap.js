8ea9df7ff916e6ec9e6d6c2d432ac845c53bae43
AKASH80047 <akp99189992@gmail.com>
2026-07-16 16:55:56 +0530

    jjnj

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,5 +38,8 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 _flutter.loader.load({
   serviceWorkerSettings: {
     serviceWorkerVersion: "956179080" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+  },
+  config: {
+    canvasKitBaseUrl: "https://www.gstatic.com/flutter-canvaskit/a10d8ac38de835021c8d2f920dbf50a920ccc030/"
   }
 });
